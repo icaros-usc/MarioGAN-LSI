010160ec52ef05c179f44b2ebc6d8aee1caf309b
Matt Fontaine <tehqin@gmail.com>
2020-03-30 14:31:48 -0700

    GridEngine script for running on kong.

---
+++ search.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Usage: gputest.sh
+# Change job name and email address as needed 
+#        
+
+# -- our name ---
+#$ -N MarioGAN-LSI
+#$ -S /bin/sh
+# Make sure that the .e and .o file arrive in the
+#working directory
+#$ -cwd
+#Merge the standard out and standard error to one file
+#$ -j y
+# Send mail at submission and completion of script
+# Specify CPU queue
+#$ -q medium
+#$ -t 1-2
+#$ -l mem_free=14.0G
+/bin/echo Running on host: `hostname`.
+/bin/echo In directory: `pwd`
+/bin/echo Starting on: `date`
+
+module load anaconda
+module list
+# Load conda
+source activate MarioGAN-LSI
+# Run the search
+python3 Search/run_search.py -w $SGE_TASK_ID -c Search/config/experiment/experiment.tml
